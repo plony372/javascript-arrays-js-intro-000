a0f725790ac8e6bc24ae8d8ec644b9decb1bcf77
plony benav <menachemfuterfas@gmail.com>
2021-02-16 18:13:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  return array.slice(0,1);
+  array.slice(0,1);
+  return array
   
 }
 
