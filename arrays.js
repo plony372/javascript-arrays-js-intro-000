7394228da6524f6d3b9abdb16fb12b519ff82571
plony benav <menachemfuterfas@gmail.com>
2021-02-16 18:02:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(0,1)
+  return array.slice(0,1)
 }
 
 // these fuctions mutate the Array
