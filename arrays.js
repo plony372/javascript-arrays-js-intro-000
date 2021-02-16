d5a51133b43c6c3d256a05596d969e6f35f71940
plony benav <menachemfuterfas@gmail.com>
2021-02-16 18:43:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -42,6 +42,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 
 function removeElementFromEndOfArray(array){
   var newarray = array.slice(0, array.length - 1)
+  return newarray;
 }
 
 // these fuctions mutate the Array
